dcca6ebf38d80af8d554d1315097c19692725d7c
vikrant shukla <vikrant.shukla@cubexo.io>
2022-02-10 19:13:06 +0530

    msg

--- user_data/views.py
+++ user_data/views.py
@@ -71,3 +71,64 @@ class ResetPasswordview(generics.UpdateAPIView):
             return Response({'status': 'password successfully changed'}, status=status.HTTP_201_CREATED)
 
         return Response({'status': 'An error occured'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+'''test'''
+
+# class RegisterAPI(APIView):
+#     def post(self, request):
+#         print("hii")
+#         serializer = UserTableSerializer(data=request.data)
+#         UserTable.objects.all().first()
+#         if serializer.is_valid():
+#             new_key = serializer.save()
+#             add_value = UserTable.objects.get(id=new_key.pk)
+#             add_value.user_id = add_value.id + 10000000
+#             add_value.save()
+#             return Response({})
+#         return Response({})
+#
+#
+# # class LoginAPI(TokenObtainPairView):
+# #     permission_classes = (AllowAny,)
+# #     serializer_class = AuthTokenSerializer
+#
+# class ProfileUpdate(generics.RetrieveUpdateAPIView):
+#     queryset = UserTable.objects.all()
+#     permission_classes = (IsAuthenticated, IsOwnerOrReadOnly,)
+#     serializer_class = UserTableSerializer
+#
+#
+# class SentMailView(generics.GenericAPIView):
+#     def post(self, request):
+#         try:
+#             user = UserTable.objects.get(email=request.data['email'])
+#         except UserTable.DoesNotExist:
+#             return Response({'error': 'Email does not exits.'})
+#         otp = Otp.objects.create(email=user)
+#
+#         otp.otp = random.randint(100000, 999999)
+#         otp.save()
+#         subject = 'Reset Your Password'
+#         to = user.email
+#         body = f'This is your OTP to reset password {otp.otp}'
+#         send_mail(subject, body, settings.EMAIL_HOST_USER, [to, ], fail_silently=False)
+#
+#         return Response({})
+#
+#
+# class ResetPasswordview(generics.UpdateAPIView):
+#     serializer_class = SetNewPasswordSerializer
+#
+#     def put(self, request, *args, **kwargs):
+#         serializer = self.serializer_class(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         request_email = request.data['email']
+#         user_object = UserTable.objects.get(email=request_email)
+#         if UserTable.objects.get(email=request_email):
+#             user_object.password = make_password(request.data['password'])
+#             user_object.save()
+#             # Otp.objects.filter(email=serializer.validated_data.get('otp')).delete()
+#             return Response({'status': 'password successfully changed'}, status=status.HTTP_201_CREATED)
+#
+#         return Response({})
