b7cf9fe5e6ed2d1c054cc458870dafd6581b9bff
vikrant shukla <vikrant.shukla@cubexo.io>
2022-02-16 15:57:15 +0530

    authntication to search game

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('tasks', app=Celery('tasks', backend='rpc://', broker='pyamqp://guest@localhost//'))
+app = Celery('tasks', backend='rpc://localhost', broker='pyamqp://guest@localhost//')
 
 
 @app.task
