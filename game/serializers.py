d4d61237a55c5caa992136dc82879778bd5e5dd5
vikrant shukla <vikrantshukla@gmail.com>
2022-03-10 15:17:09 +0530

    game model primary key

--- game/serializers.py
+++ game/serializers.py
@@ -2,17 +2,22 @@ from rest_framework import serializers
 from .models import GameModel, SearchModel
 
 
-class GameModelSerializer(serializers.Serializer):
+class GameModelSerializer(serializers.ModelSerializer):
     """for serializing data to required format"""
     user_id = serializers.CharField(max_length=20)
     # id = serializers.IntegerField()
-    player1 = serializers.CharField(max_length=100)
-    player1_score = serializers.IntegerField(default=0)
-    player2 = serializers.CharField(max_length=50)
-    dateTime = serializers.DateTimeField(format="%Y-%m-%d %H:%M %p")
-    player2_score = serializers.IntegerField(default=0)
-    player1_team = serializers.CharField(max_length=50)
-    player2_team = serializers.CharField(max_length=50)
+    # player1 = serializers.CharField(max_length=100)
+    # player1_score = serializers.IntegerField(default=0)
+    # player2 = serializers.CharField(max_length=50)
+    # dateTime = serializers.DateTimeField(format="%Y-%m-%d %H:%M %p")
+    # player2_score = serializers.IntegerField(default=0)
+    # player1_team = serializers.CharField(max_length=50)
+    # player2_team = serializers.CharField(max_length=50)
+
+    class Meta:
+        model = GameModel
+        read_only = ['id']
+        fields = "__all__"
 
     def create(self, validated_data):
         return GameModel.objects.create(**validated_data)
