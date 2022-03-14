03e93ca8c0d8786e7f7e380d7564971bc09a601c
vikantcubexo06 <97719636+vikantcubexo06@users.noreply.github.com>
2022-03-14 17:22:15 +0530

    Update settings.py

--- ManOn_backend/settings.py
+++ ManOn_backend/settings.py
@@ -150,7 +150,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
@@ -179,7 +179,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
 }
 
-# EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+# EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')UTC
 # EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 # EMAIL_PORT = os.environ.get('EMAIL_PORT')
 # EMAIL_HOST = os.environ.get('EMAIL_HOST')
