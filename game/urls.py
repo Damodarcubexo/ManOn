04e5a8badd8ecd2c5214053ae73e8f4840524140
ArvindLingwal <arvind.lingwal@cubexo.io>
2022-03-23 18:17:02 +0530

    resume game added

--- game/urls.py
+++ game/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('getgame/', views.GameView.as_view()),
     path('search/', views.SearchPlayer.as_view()),
     path('search_history/', views.SearchHistory.as_view()),
+    path('resume/', views.ResumeView.as_view()),
 ]
